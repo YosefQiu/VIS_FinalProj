67b32a4b23faf71602a08d4acf488c5ae197b41b
ZHONGYI JIANG <u1379343@lab2-31.eng.utah.edu>
2022-11-09 09:23:31 -0700

    line chart

--- js/LineChart.js
+++ js/LineChart.js
@@ -3,7 +3,7 @@ class LineChart {
     * Creates a LineChart
     * @param globalApplicationState The shared global application state (has the data and map instance in it)
     */
-    constructor(globalApplicationState) {
+    constructor(data) {
         // Set some class level variables
         this.lineChart = d3.select('#line-chart')
         this.padding = { left: 80, bottom: 150, right: 50 }
@@ -11,50 +11,65 @@ class LineChart {
         // x axis text
         this.lineChart
             .append('text')
-            .text('Date')
+            .text('Year')
             .attr('x', 350)
             .attr('y', 500);
 
         // Append y axis text
         this.lineChart
             .append('text')
-            .text('Case per million')
+            .text('Trading Amount in US dolloar')
             .attr('x', -250)
             .attr('y', 25)
             .attr('transform', 'rotate(-90)');
 
-        this.click = false
-        this.lineChart.on('click', (d, data) => {
-            this.click = true
-        })
+        let country = "China"
+        let TradingType = "Import"
+        let LineChartData = data.filter(v => v.country === country && v.tradeType === TradingType);
 
-        this.updateLineChart(country);
+        let temp_data=[]
+        LineChartData.forEach(element => {
+            element.TradeData.forEach((data,i)=>{
+                let year = String(i+1992)
+                let temp={
+                    country: element.country,
+                    traders: element.traders,
+                    tradeType: element.tradeType,
+                    year: year,
+                    data:data
+                }
+                //temp[year] = data
+                temp_data.push(temp)
+                
+            })
+        });
+        this.updateLineChart(temp_data);
     }
-    updateLineChart(locations) {
+    updateLineChart(LineChartData) {
+        //console.log(LineChartData)
         this.lineChart = d3.select('#line-chart')
-        let trading = globalApplicationState.data;
-        // get importing/exporting, country
-        let dataLookup = [];
-        let format = d3.timeParse("%Y");
+  
+        // current linechart data should be an array of length 9-10 (countries)
 
-    
-        let groupedLocationData = d3.group(dataLookup, d => d.iso_code)
+        // year: 1992-2020
+
+        let groupedLocationData = d3.group(LineChartData, d => d.traders)
         this.lineColorScale = d3.scaleOrdinal(d3.schemeTableau10).domain(groupedLocationData.keys());
 
         // Add x axis --> it is a date format
-        const xAxis = d3.scaleTime()
-            .domain(d3.extent(dataLookup.map((row) => row.date)))
+        const xAxis = d3.scaleLinear()
+            .domain([1992,2020])
             .range([this.padding.left, 700 - this.padding.right])
             .nice();
-
+        
         this.lineChart
             .select('#x-axis')
             .attr('transform', `translate(0, ${600 - this.padding.bottom})`)
-            .call(d3.axisBottom(xAxis).tickFormat(d3.timeFormat("%b %Y")))
+            .call(d3.axisBottom(xAxis))
 
         // y axis, linear float number
         const yAxis = d3.scaleLinear()
-            .domain([0, Math.max(...dataLookup.map((row) => row.total_cases_per_million))])
+            .domain([Math.min(...LineChartData.map((row) => row.data)), Math.max(...LineChartData.map((row) => row.data))])
             .range([600 - this.padding.bottom, 10])
             .nice();
 
@@ -62,61 +77,61 @@ class LineChart {
             .attr('transform', `translate(${this.padding.left},0)`)
             .call(d3.axisLeft(yAxis));
 
-
+        //console.log(groupedLocationData)
         this.lineChart
             .select('#lines')
             .selectAll('path')
             .data(groupedLocationData)
             .join('path')
             .attr('fill', 'none')
-            .attr('stroke', ([group, values]) => this.lineColorScale(values[0].location))
+            .attr('stroke', ([group, values]) => this.lineColorScale(values[0].traders))
             .attr('stroke-width', 1)
             .attr('d', ([group, values]) => d3.line()
-                .x((d) => xAxis(d.date))
-                .y((d) => yAxis(d.total_cases_per_million))
+                .x((d,i) =>xAxis(parseInt(d.year)))
+                .y((d) => (d3.format(".2s")(yAxis(parseFloat(d.data)))))
                 (values))
 
-        // interative section
-        this.lineChart.on('mousemove', (event) => {
-            if (event.offsetX > this.padding.left && event.offsetX < 700 - this.padding.right && this.click) {
-                // Set the line position
+        // // interative section
+        // this.lineChart.on('mousemove', (event) => {
+        //     if (event.offsetX > this.padding.left && event.offsetX < 700 - this.padding.right && this.click) {
+        //         // Set the line position
 
-                this.lineChart
-                    .select('#overlay')
-                    .select('line')
-                    .attr('stroke', 'black')
-                    .attr('x1', event.offsetX)
-                    .attr('x2', event.offsetX)
-                    .attr('y1', 600 - this.padding.bottom)
-                    .attr('y2', 0);
+        //         this.lineChart
+        //             .select('#overlay')
+        //             .select('line')
+        //             .attr('stroke', 'black')
+        //             .attr('x1', event.offsetX)
+        //             .attr('x2', event.offsetX)
+        //             .attr('y1', 600 - this.padding.bottom)
+        //             .attr('y2', 0);
 
-                //// round date data 
-                const dateHovered = xAxis.invert(event.offsetX)
-                dateHovered.setTime(dateHovered.getTime() + (12 * 60 * 60 * 1000));
-                dateHovered.setHours(0, 0, 0, 0);
+        //         //// round date data 
+        //         const dateHovered = xAxis.invert(event.offsetX)
+        //         dateHovered.setTime(dateHovered.getTime() + (12 * 60 * 60 * 1000));
+        //         dateHovered.setHours(0, 0, 0, 0);
 
-                // sort data
-                const filteredData = dataLookup
-                    .filter((row) => +row.date === +dateHovered)
-                    .sort((rowA, rowB) => rowB.total_cases_per_million - rowA.total_cases_per_million)
+        //         // sort data
+        //         const filteredData = dataLookup
+        //             .filter((row) => +row.date === +dateHovered)
+        //             .sort((rowA, rowB) => rowB.total_cases_per_million - rowA.total_cases_per_million)
 
-                // flag to switch side
-                let condition = event.offsetX > 500 - this.padding.right
+        //         // flag to switch side
+        //         let condition = event.offsetX > 500 - this.padding.right
 
-                // draw text labels
-                this.lineChart.select('#overlay')
-                    .selectAll('text')
-                    .data(filteredData)
-                    .join('text')
-                    //.text(d => d.total_cases_per_million > 1000 ? `${d.location}, ${Math.round(d.total_cases_per_million / 1000)}k` : `${d.location}, ${Math.round(d.total_cases_per_million)}`)
-                    .text(d => `${d.location}, ${d3.format(".2s")(d.total_cases_per_million)}`)
-                    .attr('x', condition ? event.offsetX - 150 : event.offsetX + 20)
-                    .attr('y', (d, i) => 20 * i + 20)
-                    .attr('alignment-baseline', 'hanging')
-                    .attr('fill', (d) => this.lineColorScale(d.location));
-            }
+        //         // draw text labels
+        //         this.lineChart.select('#overlay')
+        //             .selectAll('text')
+        //             .data(filteredData)
+        //             .join('text')
+        //             //.text(d => d.total_cases_per_million > 1000 ? `${d.location}, ${Math.round(d.total_cases_per_million / 1000)}k` : `${d.location}, ${Math.round(d.total_cases_per_million)}`)
+        //             .text(d => `${d.location}, ${d3.format(".2s")(d.total_cases_per_million)}`)
+        //             .attr('x', condition ? event.offsetX - 150 : event.offsetX + 20)
+        //             .attr('y', (d, i) => 20 * i + 20)
+        //             .attr('alignment-baseline', 'hanging')
+        //             .attr('fill', (d) => this.lineColorScale(d.location));
+        //     }
 
-        });
+        // });
     }
 
 }
